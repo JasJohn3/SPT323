ab67371826807438d4da63925d8c5d5c5b2e1949
Jason Lane Johnson <jasjohn3@uat.edu>
2020-02-15 12:48:23 -0700

    React Navigation

--- spt323-tutorial-app/App.js
+++ spt323-tutorial-app/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+import HomeScreen from './Navigation/HomeScreen';
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+});
+
+export default class App extends React.Component {
+  render()
+  {
+    return ( <AppNavigator/>);
+  }
+  
 }
 
 const styles = StyleSheet.create({
